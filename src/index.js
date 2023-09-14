96822f0a333f0ef3a833dae5d357f3d0cedd8abc
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-14 14:57:16 +0100

    Made init into an IIFE & import event eventController
    Made the main block into an IIFE for security reasons. Added an event
    listener to all buttons that send event to an imported event handler.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import './style.css';
 import domStarter from './domStarter.js';
-import domBoardOptns from './domBoardOptns';
+import domBoardOptns from './domBoardOptns.js';
+import eventController from './eventController.js';
 
-domStarter();
-domBoardOptns();
+const init = (function(){
+
+    //Start outter dom
+    domStarter();
+    //Start board options dom
+    domBoardOptns();
+
+    //Cache options DOM
+    const btns = document.querySelectorAll('button');
+
+    //Set event listeners
+    btns.forEach(btn => btn.addEventListener('click', (event) => eventController(event)));
+
+})();
