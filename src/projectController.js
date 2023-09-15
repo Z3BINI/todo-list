fa692d680c16a6f56c9b0555bb22367b62c22a9e
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-15 16:58:00 +0100

    Change loaded project board element on select change
    Every time user changes the select value the board automatically loads.
    Also changed all instances of the variable to be named project. user
    can add project boards and name them.

--- src/projectController.js
+++ src/projectController.js
@@ -1,21 +1,35 @@
 //Initialize the projects array
 const projects = [];
 
-const eventManager = function(eventType) {
+const eventManager = function(event) { 
 
-    if (eventType === 'add-board') {
-        boardMaker(prompt('Name your project:'));
-        renderBoardOptns();
+    //Cache Dom here
+    const selector = document.querySelector('select');
+    const projectWrapper = document.querySelector('.board');
+
+    if (event.target.id === 'add-project') {
+        projectMaker(prompt('Name your project:'));
+        addProjectsToSelector(selector);
         return;
-    };
+    }
+
+    if (event.target.id === 'project-change') {
+        clearCurrentProject(projectWrapper);
+        renderSelectedProject(event.target.value, projectWrapper);
+        return;
+    }
+
+    if (event.target.id === 'del-project') {
+        console.log('delete');
+    }
 
 }
 
-const boardMaker = function(name) {
+const projectMaker = function(name) {
 
     //Create new project board element
     const projectBoardElement = document.createElement('div');
-    projectBoardElement.classList.add('board');
+    projectBoardElement.classList.add('board-' + name);
     projectBoardElement.value = name;
     
     //Create new project board option for the select
@@ -33,15 +47,18 @@ const boardMaker = function(name) {
 
 }
 
-const renderBoardOptns = function() {
+const addProjectsToSelector = (selector) => projects.forEach(project => selector.add(project.option));
 
-    //Cache the select element
-    const selector = document.querySelector('select');
+const clearCurrentProject = (projectWrapper) => projectWrapper.innerHTML = '';
 
-    projects.forEach(project => {
-        selector.add(project.option);
-    });
+const renderSelectedProject = function(boardName, projectWrapper) { 
+
+    const searchSelected = projects.filter(project => project.option.value === boardName);
     
+    if (searchSelected.length === 1) {
+        projectWrapper.appendChild(searchSelected[0].element);
+    }
+
 }
 
 export { eventManager }
