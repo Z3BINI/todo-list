dd1e727de55dfa76b7fa2d20001c8750fd9f6906
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-18 18:09:45 +0100

    Added object literals to hold functions and fixed delete problem
    Object literal modules to hold the render and logical related functions.
    Fixed issue of when deleting the last board the options of select element
    wouldn't change anything anymore.

--- src/projectController.js
+++ src/projectController.js
@@ -1,36 +1,94 @@
-//Initialize the projects array
+//Init projects array
 let projects = [];
 
-const eventManager = function(event) { 
+export default function(event) { 
 
-    //Cache Dom here
+    //Cache Dom 
     const selector = document.querySelector('select');
     const projectWrapper = document.querySelector('.board');
     const currentLoadedProject = document.querySelector('.board > div');
 
+    //Render module functions
+    const render = {
+
+        addProjectsToSelector: () =>  projects.forEach(project => selector.add(project.option)),
+        clearProjectsInSelector: () => selector.innerHTML = '',
+        addDefaultOptnInSelector: () => {
+            const defaultOption = document.createElement('option');
+            
+            defaultOption.text = 'Select a project...';
+            defaultOption.selected = true;
+            defaultOption.disabled = true;
+            defaultOption.hidden = true;
+        
+            selector.add(defaultOption, 0);
+        },
+        clearCurrentProject: () => projectWrapper.innerHTML = '',
+        selectedProject: (boardName) => {
+            const searchSelected = controller.searchProject(boardName);
+    
+            if (searchSelected.length === 1) {
+                return projectWrapper.appendChild(searchSelected[0].element);
+            }
+        }
+
+    }
+
+    //Logic module functions
+    const controller = {
+
+        projectMaker: (className, name) => {
+            //Create new project board element
+            const projectBoardElement = document.createElement('div');
+            projectBoardElement.classList.add('board-' + className);
+            projectBoardElement.value = className;
+            
+            //Create new project board option for the select
+            const boardOption = document.createElement('option');
+            boardOption.text = name;
+            boardOption.value = className;
+        
+            //Unite them in an object
+            const boardObj = {
+                element: projectBoardElement,
+                option: boardOption
+            }
+        
+            return projects.push(boardObj);
+        },
+
+        deleteCurrentProject: () => {
+            projects = projects.filter( project => project.element.value !== currentLoadedProject.value)
+        },
+
+        searchProject: (needle) => projects.filter(project => project.option.value === needle)
+
+    }
+    
+    //Event checker
     if (event.target.id === 'add-project') {
         const projectName = prompt('Name your project:');
         const projectClassName = projectName.replaceAll(' ', '-');
 
-        projectMaker(projectClassName, projectName);
-        addProjectsToSelector(selector);
+        controller.projectMaker(projectClassName, projectName);
+        render.addProjectsToSelector();
         return;
     }
 
     if (event.target.id === 'project-change') {
-        clearCurrentProject(projectWrapper);
-        renderSelectedProject(event.target.value, projectWrapper);
+        render.clearCurrentProject();
+        render.selectedProject(event.target.value);
         return;
     }
 
     if (event.target.id === 'del-project') {
         if (!currentLoadedProject) return alert('No project selected!'); 
         if (confirm('This will permanently delete the board, are you sure?')) {
-            deleteCurrentProject(currentLoadedProject);
-            clearCurrentProject(projectWrapper);
-            clearSelectorProjects(selector);
-            addProjectsToSelector(selector);
-            console.log(projects);
+            controller.deleteCurrentProject();
+            render.clearCurrentProject();
+            render.clearProjectsInSelector();
+            render.addDefaultOptnInSelector();
+            render.addProjectsToSelector();
             return;
         }
         return;
@@ -38,57 +96,8 @@ const eventManager = function(event) {
 
 }
 
-const projectMaker = function(className, name) {
 
-    //Create new project board element
-    const projectBoardElement = document.createElement('div');
-    projectBoardElement.classList.add('board-' + className);
-    projectBoardElement.value = className;
-    
-    //Create new project board option for the select
-    const boardOption = document.createElement('option');
-    boardOption.text = name;
-    boardOption.value = className;
 
-    //Unite them in an object
-    const boardObj = {
-        element: projectBoardElement,
-        option: boardOption
-    }
-
-    return projects.push(boardObj);
-
-}
-
-const addProjectsToSelector = (selector) => projects.forEach(project => selector.add(project.option));
-
-const clearSelectorProjects = (selector) => {
-    Array.from(selector).splice(0);
-    console.log(selector);
-}
-
-const clearCurrentProject = (projectWrapper) => projectWrapper.innerHTML = '';
-
-const renderSelectedProject = function(boardName, projectWrapper) { 
-
-    const searchSelected = searchProject(boardName);
-    
-    if (searchSelected.length === 1) {
-        return projectWrapper.appendChild(searchSelected[0].element);
-    }
-
-}
-
-const deleteCurrentProject = function(currentLoadedProject) {
-    
-    projects = projects.filter( project => project.element.value !== currentLoadedProject.value);
-    
-    return projects;
-}
-
-const searchProject = (needle) => projects.filter(project => project.option.value === needle);
-
-export { eventManager }
     
 
 
