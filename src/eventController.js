ad4e746e32bec9a7e5d336497ca5f5f01b1f49c3
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-21 15:11:03 +0100

    Import task controller and check for add task

--- src/eventController.js
+++ src/eventController.js
@@ -1,7 +1,7 @@
 import projectEventManager from './projectController.js';
-
+import taskEventManager from './taskController.js';
 
 export default function(event) {
     if (event.target.id.includes('project')) projectEventManager(event);
-    
-}
+    if (event.target.id.includes('card')) taskEventManager(event);
+} 
