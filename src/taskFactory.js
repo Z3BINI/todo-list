9dba3b1f1bfd9fd367174484add49a0c2704080e
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-21 15:11:36 +0100

    Basic task making class

---
+++ src/taskFactory.js
@@ -0,0 +1,16 @@
+export default class Task {
+
+    constructor(title, designation, importance, status){
+        this.title = title;
+        this.designation = designation;
+        this.importance = importance;
+        this.status = status;
+    }
+
+    testFunk() {
+        console.log('pp');
+    }
+        
+}
+
+
