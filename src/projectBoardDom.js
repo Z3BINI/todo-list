4ffa0376fde53de0e89591f15ab1750668a753c1
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-21 14:36:09 +0100

    Added container titles

--- src/projectBoardDom.js
+++ src/projectBoardDom.js
@@ -1,14 +1,16 @@
 export default function(className) {
     const projectBoardElement = document.createElement('div');
-
     const undefContainer = document.createElement('div');
-
     const doingDoneContainer = document.createElement('div');
-    
     const doingContainer = document.createElement('div');
     const doneContainer = document.createElement('div');
 
+    const undefTitle = document.createElement('h3');
+    const doingTitle = document.createElement('h3');
+    const doneTitle = document.createElement('h3');
+
     undefContainer.id = 'undef-container';
+    doingDoneContainer.id = 'doing-done-container';
     doingContainer.id = 'doing-container';
     doneContainer.id = 'done-container';
 
@@ -17,6 +19,14 @@ export default function(className) {
 
 
     projectBoardElement.value = className;
+
+    undefTitle.textContent = 'Undefined tasks';
+    doingTitle.textContent = 'Doing';
+    doneTitle.textContent = 'Done';
+
+    undefContainer.appendChild(undefTitle);
+    doingContainer.appendChild(doingTitle);
+    doneContainer.appendChild(doneTitle);
 
 
     projectBoardElement.appendChild(undefContainer);
