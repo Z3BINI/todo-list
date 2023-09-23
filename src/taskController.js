be8f7a3e826f37fb54fc0be25a3a779ae159566a
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-23 19:12:49 +0100

    Added render & control methods, send board and cardelement to factory

--- src/taskController.js
+++ src/taskController.js
@@ -1,3 +1,6 @@
+import taskFactory from './taskFactory.js';
+import taskDom from './taskDom.js';
+
 export default function(event){
     
     //Prevent page refresh case form btns are pressed
@@ -8,17 +11,54 @@ export default function(event){
 
     const render = {
         showForm: () => dialogForm.showModal(),
-        closeForm: () => dialogForm.close()
+        closeForm: () => dialogForm.close(),
+        resetForm: () => dialogForm.children[0].reset(),
+        resetSelects: () => {
+            const selects = document.querySelectorAll('form select');
+
+            selects.forEach(select => {
+                select.options[0].selected = true;
+            });
+        }
+
     }
 
+    const control = {
+        getUserInput: () => {
+
+            const title = document.querySelector('#title');
+            const designation = document.querySelector('#designation');
+            const status = document.querySelector('#status');
+            const importance = document.querySelector('#importance');
+
+            return [title.value, designation.value, status.value, importance.value];
+        }
+    }
+
+    //Get current loaded board, if any
+    const currentBoard = document.querySelector('.board > div');
+
     if (event.target.id === 'insert-task') {
-        
+
+        //https://stackoverflow.com/questions/44372822/preventdefault-stops-form-validation --> Form validation with preventDefault()
+
+        const newTaskArr = control.getUserInput();
+        const taskElement = taskDom(newTaskArr);
+
+        newTaskArr.push(currentBoard);
+        newTaskArr.push(taskElement);
+
+        render.closeForm();
+        render.resetForm();
+        render.resetSelects();
+
+        const newTask = new taskFactory(newTaskArr);
+
+        newTask.place();
+        console.log(newTask);
     }
 
     if (event.target.id === 'add-task') {
-
-        //Get current loaded board, if any
-        const currentBoard = document.querySelector('.board > div');
 
         if (!currentBoard) return alert('No board selected to add task!');  
 
