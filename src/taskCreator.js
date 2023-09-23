a20f4afe5887b47c9f7e3af87846f1a7493223ab
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-23 15:03:38 +0100

    Add event listener to all new task btns and send to event controller

--- src/taskCreator.js
+++ src/taskCreator.js
@@ -1,7 +1,8 @@
 import taskFactory from './taskFactory.js';
 import dialogElement from './dialogFormDom.js';
+import eventController from './eventController.js';
 
-export default function(event){
+export default function(){
 
     //Get current loaded board, if any
     const currentBoard = document.querySelector('.board > div');
@@ -14,6 +15,12 @@ export default function(event){
 
         //Show the form for a new task to be added
         dialogForm.showModal();
+
+        //Cache dialog buttons
+        const dialogBtns = document.querySelectorAll('form > div > button');
+
+        //Send any click to event controller
+        dialogBtns.forEach(dialogBtn => dialogBtn.addEventListener('click', (event) => eventController(event)));
 
         //use data to create new task on factory
         //render new data with DOM renderer
