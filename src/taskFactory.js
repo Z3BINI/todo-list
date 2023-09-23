8c39925b109fbb70ca5e7475111d3c283b5e1774
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-23 19:11:42 +0100

    Changed parameter to array, added placement function

--- src/taskFactory.js
+++ src/taskFactory.js
@@ -1,10 +1,16 @@
 export default class Task {
 
-    constructor(title, designation, importance, status){
-        this.title = title;
-        this.designation = designation;
-        this.importance = importance;
-        this.status = status;
+    constructor(newTaskArr){
+        this.title = newTaskArr[0];
+        this.designation = newTaskArr[1];
+        this.status = newTaskArr[2];
+        this.importance = newTaskArr[3];
+        this.board = newTaskArr[4];
+        this.card = newTaskArr[5];
+    }
+
+    place() {
+        if (this.status = 'Undefined') this.board.children[0].appendChild(this.card);     
     }
 
     delete() {
