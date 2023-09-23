1d3e276b8ffeeed7e2c605e9a940ea0a7648999e
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-23 15:55:16 +0100

    Event controller simplified due to new logic organization

--- src/eventController.js
+++ src/eventController.js
@@ -1,10 +1,7 @@
 import projectEventManager from './projectController.js';
-import taskCreator from './taskCreator.js';
 import taskEventManager from './taskController.js';
-import dialogFormDom from './dialogFormDom.js';
 
 export default function(event) {
     if (event.target.id.includes('project')) projectEventManager(event);
-    if (event.target.id.includes('card')) taskCreator(event);
     if (event.target.id.includes('task')) taskEventManager(event);
 } 
