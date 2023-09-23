a2d70b641c09cd656d137a4e596098a63c576f85
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-23 15:02:49 +0100

    Added new even possibility, clarified module names

--- src/eventController.js
+++ src/eventController.js
@@ -1,7 +1,10 @@
 import projectEventManager from './projectController.js';
-import taskEventManager from './taskCreator.js';
+import taskCreator from './taskCreator.js';
+import taskEventManager from './taskController.js';
+import dialogFormDom from './dialogFormDom.js';
 
 export default function(event) {
     if (event.target.id.includes('project')) projectEventManager(event);
-    if (event.target.id.includes('card')) taskEventManager(event);
+    if (event.target.id.includes('card')) taskCreator(event);
+    if (event.target.id.includes('task')) taskEventManager(event);
 } 
