50552ab5cfc9b09b57efa17e7a02fbfb60399980
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-28 16:37:42 +0100

    Add and execute task option funcs

--- src/taskFactory.js
+++ src/taskFactory.js
@@ -13,18 +13,36 @@ export default class Task {
         if (this.status === 'Undefined') this.board.children[0].children[1].appendChild(this.card);     
         if (this.status === 'Doing') this.board.children[1].children[0].children[1].appendChild(this.card);
         if (this.status === 'Done') this.board.children[1].children[1].children[1].appendChild(this.card);
+
+        if (this.importance === 'Low') this.card.style = 'border: 2px solid blue;';
+        if (this.importance === 'Medium') this.card.style = 'border: 2px solid yellow;';
+        if (this.importance === 'High') this.card.style = 'border: 2px solid red;';
     }
 
-    delete() {
-        console.log('delete');
+    addEventListeners() {
+        //Store the card's option elements
+        const delBtn = this.card.querySelector('button.del');
+        const changeStatusSelect = this.card.querySelector('select.change-stat');
+        const changeImportanceSelect = this.card.querySelector('select.change-imp');
+
+        delBtn.addEventListener('click', () => this.delete(this.card));
+        changeStatusSelect.addEventListener('change', (event) => this.changeStatus(this, event));
+        changeImportanceSelect.addEventListener('change', (event) => this.changeImportance(this, event));
+
     }
 
-    changeStatus() {
-        console.log('change status');
+    delete(card) {
+        card.remove();
     }
 
-    changeImportance() {
-        console.log('change importance');
+    changeStatus(card, event) {
+        card.status = event.target.value;
+        card.place();    
+    }
+
+    changeImportance(card, event) {
+        card.importance = event.target.value;
+        card.place();
     }
 
         
